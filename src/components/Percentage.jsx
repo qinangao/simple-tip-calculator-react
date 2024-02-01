0c356a56d5cdd25e8bf77d4e937863992cfbfb73
Qinan Gao <qinanagao@gmail.com>
2024-02-01 22:42:38 +0000

    add oncharge

--- src/components/Percentage.jsx
+++ src/components/Percentage.jsx
@@ -1,12 +1,15 @@
-function Percentage({ question, percentage }) {
+function Percentage({ question, percentage, setPercentage }) {
   return (
     <div>
       {question}
-      <select>
+      <select
+        value={percentage}
+        onChange={(e) => setPercentage(Number(e.target.value))}
+      >
         <option value="0">Dissatisfied (0%)</option>
-        <option value="5">It was okay (5%)</option>
-        <option value="10">It was good (10%)</option>
-        <option value="20">Absolutely amazing (20%)</option>
+        <option value="0.05">It was okay (5%)</option>
+        <option value="0.1">It was good (10%)</option>
+        <option value="0.2">Absolutely amazing (20%)</option>
       </select>
     </div>
   );
